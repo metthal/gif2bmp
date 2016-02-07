f9f2bdb2c57a07bebaa31cfd07b32d8a17c768e9
Marek Milkovič <milkovic.marek@gmail.com>
2016-02-07 19:31:27 +0100

    Refactoring of LZW decoder

--- lzw_decoder.cpp
+++ lzw_decoder.cpp
@@ -5,108 +5,60 @@
 #include "lzw_decoder.h"
 
 LzwDecoder::LzwDecoder(std::uint8_t firstCodeSize, std::uint16_t codeTableSize, const DataBuffer& codedData) :
-	_firstCodeSize(firstCodeSize), _codeTableSize(codeTableSize), _codeTable(), _codedData(codedData), _lastCode(nullptr)
+	_firstCodeSize(firstCodeSize), _codeSize(firstCodeSize), _readPos(0), _initCodeTableSize(codeTableSize), _codeTable(), _codedData(codedData), _lastCode(nullptr)
 {
 }
 
 bool LzwDecoder::decode(DataBuffer& decodedData)
 {
-	std::uint64_t readPos = 0;
-	std::uint8_t codeSize = _firstCodeSize;
-
-	// Not enough bits in coded data
-	if (readPos + codeSize >= (_codedData.getSize() * 8))
+	std::uint16_t code;
+	if (!getNextCode(code))
 		return false;
-	std::uint16_t firstCode = _codedData.readBits(readPos, codeSize).getInt<std::uint16_t>();
-	readPos += codeSize;
 
 	// First code needs to be always reset
-	if (!isResetCode(firstCode))
+	if (!isResetCode(code))
 		return false;
-	resetCodeTable(codeSize);
 
-	// Not enough bits in coded data
-	if (readPos + codeSize >= (_codedData.getSize() * 8))
+	if (!resetCodeTable(decodedData))
 		return false;
-	firstCode = _codedData.readBits(readPos, codeSize).getInt<std::uint16_t>();
-	readPos += codeSize;
-
-	// Code after reset is just read, its value should be in code table, index is written to output and it is remembered as last code
-	Code* firstCodeObj = isInCodeTable(firstCode);
-	if (firstCodeObj == nullptr)
-		return false;
-	decodedData.append(firstCodeObj->data);
-	_lastCode = firstCodeObj;
 
 	while (true)
 	{
-		// Not enough bits in coded data
-		if (readPos + codeSize >= (_codedData.getSize() * 8))
+		if (!getNextCode(code))
 			return false;
 
-		Code* codeObj = nullptr;
-		std::uint16_t code = _codedData.readBits(readPos, codeSize).getInt<std::uint16_t>();
-		readPos += codeSize;
+		// Find the code in code table
+		Code* codeObj = isInCodeTable(code);
 
-		std::cout << "Current code: " << code
-			<< " last code: " << (_lastCode ? _lastCode->index : -1)
-			<< std::endl;
 		if (isEndCode(code))
 		{
 			break;
 		}
 		else if (isResetCode(code))
 		{
-			resetCodeTable(codeSize);
-
-			// Not enough bits in coded data
-			if (readPos + codeSize >= (_codedData.getSize() * 8))
+			if (!resetCodeTable(decodedData))
 				return false;
-			code = _codedData.readBits(readPos, codeSize).getInt<std::uint16_t>();
-			readPos += codeSize;
 
-			// Code after reset is just read, its value should be in code table, index is written to output and it is remembered as last code
-			codeObj = isInCodeTable(code);
-			if (codeObj == nullptr)
-				return false;
-			decodedData.append(codeObj->data);
-			_lastCode = codeObj;
 			continue;
 		}
-		else if ((codeObj = isInCodeTable(code)) != nullptr)
+		// Existing code
+		else if (codeObj != nullptr)
 		{
 			assert(_lastCode);
 			decodedData.append(codeObj->data);
 
-			Code newCode;
-			newCode.index = _codeTable.size();
-			newCode.data = _lastCode->data;
-			newCode.data.push_back(codeObj->data[0]);
-			_codeTable.insert( { newCode.index, newCode } );
-			std::cout << "Added #" << newCode.index << " ";
-			std::for_each(newCode.data.begin(), newCode.data.end(), [](std::uint8_t x) { std::cout << static_cast<std::uint16_t>(x) << ","; });
-			std::cout << std::endl;
-
-			if (newCode.index == ((1 << codeSize) - 1))
-				codeSize++;
+			// For existing code, create new code with the first free index, what is size of code table
+			createNewCode(_codeTable.size(), codeObj->data[0]);
 		}
+		// New code
 		else
 		{
 			assert(_lastCode);
 			decodedData.append(_lastCode->data);
 			decodedData.append(_lastCode->data[0]);
 
-			Code newCode;
-			newCode.index = _codeTable.size();
-			newCode.data = _lastCode->data;
-			newCode.data.push_back(_lastCode->data[0]);
-			_codeTable.insert( { newCode.index, newCode } );
-			std::cout << "Added #" << newCode.index << " ";
-			std::for_each(newCode.data.begin(), newCode.data.end(), [](std::uint8_t x) { std::cout << static_cast<std::uint16_t>(x) << ","; });
-			std::cout << std::endl;
-
-			if (newCode.index == ((1 << codeSize) - 1))
-				codeSize++;
+			// We need to create new code with this non-existing code index
+			createNewCode(code, _lastCode->data[0]);
 		}
 
 		_lastCode = isInCodeTable(code);
@@ -118,7 +70,7 @@ bool LzwDecoder::decode(DataBuffer& decodedData)
 bool LzwDecoder::isResetCode(std::uint16_t code)
 {
 	if (_codeTable.empty())
-		return code == _codeTableSize;
+		return code == _initCodeTableSize;
 
 	Code* codeObj = isInCodeTable(code);
 	return codeObj ? codeObj->reset : false;
@@ -127,19 +79,29 @@ bool LzwDecoder::isResetCode(std::uint16_t code)
 bool LzwDecoder::isEndCode(std::uint16_t code)
 {
 	if (_codeTable.empty())
-		return code == _codeTableSize + 1;
+		return code == _initCodeTableSize + 1;
 
 	Code* codeObj = isInCodeTable(code);
 	return codeObj ? codeObj->end : false;
 }
 
-void LzwDecoder::resetCodeTable(uint8_t& codeSize)
+bool LzwDecoder::getNextCode(std::uint16_t& code)
 {
-	codeSize = _firstCodeSize;
-	//_lastCode = nullptr;
+	// Not enough bits in coded data
+	if (_readPos + _codeSize > (_codedData.getSize() * 8))
+		return false;
+
+	code = _codedData.readBits(_readPos, _codeSize).getInt<std::uint16_t>();
+	_readPos += _codeSize;
+	return true;
+}
+
+bool LzwDecoder::resetCodeTable(DataBuffer& decodedData)
+{
+	_codeSize = _firstCodeSize;
 
 	_codeTable.clear();
-	for (std::uint16_t i = 0; i < _codeTableSize; ++i)
+	for (std::uint16_t i = 0; i < _initCodeTableSize; ++i)
 	{
 		Code newCode;
 		newCode.reset = newCode.end = false;
@@ -148,19 +110,50 @@ void LzwDecoder::resetCodeTable(uint8_t& codeSize)
 		_codeTable.insert( { newCode.index, newCode } );
 	}
 
+	// Clear code
 	Code resetCode;
 	resetCode.reset = true;
 	resetCode.end = false;
-	resetCode.index = _codeTableSize;
+	resetCode.index = _initCodeTableSize;
 	resetCode.data = {};
 	_codeTable.insert( { resetCode.index, resetCode } );
 
+	// End of Information code
 	Code endCode;
 	endCode.reset = false;
 	endCode.end = true;
-	endCode.index = _codeTableSize + 1;
+	endCode.index = _initCodeTableSize + 1;
 	endCode.data = {};
 	_codeTable.insert( { endCode.index, endCode } );
+
+	// Not enough bits in coded data
+	std::uint16_t code;
+	if (!getNextCode(code))
+		return false;
+
+	// Code after reset is just read, its value should be in code table, index is written to output and it is remembered as last code
+	Code* codeObj = isInCodeTable(code);
+	if (codeObj == nullptr)
+		return false;
+
+	decodedData.append(codeObj->data);
+	_lastCode = codeObj;
+	return true;
+}
+
+void LzwDecoder::createNewCode(std::uint16_t index, std::uint8_t appendByte)
+{
+	Code newCode;
+	newCode.index = index;
+	newCode.data = _lastCode->data;
+	newCode.data.push_back(appendByte);
+	_codeTable.insert( { newCode.index, newCode } );
+
+	// If the new index is maximum value on _codeSize number of bytes and _codeSize is less than 12
+	//   then we need to increase the _codeSize
+	if (newCode.index >= ((1 << _codeSize) - 1))
+		if (_codeSize < MAX_CODE_SIZE)
+			_codeSize++;
 }
 
 LzwDecoder::Code* LzwDecoder::isInCodeTable(std::uint16_t code)
--- lzw_decoder.h
+++ lzw_decoder.h
@@ -7,6 +7,8 @@
 
 #include "data_buffer.h"
 
+const std::uint16_t MAX_CODE_SIZE = 12;
+
 class LzwDecoder
 {
 public:
@@ -31,12 +33,17 @@ protected:
 	bool isResetCode(std::uint16_t code);
 	bool isEndCode(std::uint16_t code);
 
+	bool getNextCode(std::uint16_t& code);
+	void createNewCode(std::uint16_t index, std::uint8_t appendByte);
+
 	Code* isInCodeTable(std::uint16_t code);
-	void resetCodeTable(std::uint8_t& codeSize);
+	bool resetCodeTable(DataBuffer& decodedData);
 
 private:
 	std::uint8_t _firstCodeSize;
-	std::uint16_t _codeTableSize;
+	std::uint8_t _codeSize;
+	std::uint64_t _readPos;
+	std::uint16_t _initCodeTableSize;
 	CodeTable _codeTable;
 	DataBuffer _codedData;
 	Code* _lastCode;
